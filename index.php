4fba9de1afb02fa7b83b7411202ecdfb11fc71a6
Brigham Heyn <brigham@rotowire.com>
2024-04-17 02:28:07 -0500

    finishing touches

--- index.php
+++ index.php
@@ -10,30 +10,40 @@ use XES\CodeChallenge\Model\SearchBy;
 use XES\CodeChallenge\View\FilterBy;
 use XES\CodeChallenge\View\CountrySearchInput;
 use XES\CodeChallenge\View\CountryTable;
+use XES\CodeChallenge\View\HighlightedText;
+use XES\CodeChallenge\View\SortOrder;
+use XES\CodeChallenge\View\SortBy;
 
 use const XES\CodeChallenge\Model\DEFAULT_SEARCH_BY;
 
 $term = @trim($_GET['q']);
 $filteringBy = array_filter(array_map(fn($f) => FilterBy::tryFrom($f), @$_GET["f"] ?? []), fn($f) => $f != null);
 $searchingBy = array_filter(array_map(fn($s) => SearchBy::tryFrom($s), @$_GET['s'] ?? array_map(fn($by) => $by->value, DEFAULT_SEARCH_BY)), fn($by) => $by != null);
+$sortBy = SortBy::tryFrom(@$_GET["t"]) ?? SortBy::Name;
+$sortOrder = SortOrder::tryFrom(@$_GET["o"]) ?? SortOrder::Asc;
 $useCustomSearch = (bool)@$_GET['c'];
 
 $client = new Client();
-
 $countries = match ($useCustomSearch) {
     true => (new Countries($client->all()))->search($term, $searchingBy),
     false => $client->search($term, $searchingBy)
 };
 
-$input = new CountrySearchInput($term, $filteringBy, $searchingBy, $useCustomSearch);
-$tbl = new CountryTable($countries, $filteringBy);
+$input = new CountrySearchInput($term, $searchingBy, $useCustomSearch);
+$tbl = new CountryTable($countries, $filteringBy, $sortBy, $sortOrder);
 
 ?>
 
 <!doctype html>
 <html lang="en-US">
     <head>
+        <link rel="stylesheet" href="basic.css">
         <style>
+            body {
+                width: 900px;
+                margin: 40px auto 0 auto;
+            }
+
             img {
                 object-fit: contain;
                 display: block;
@@ -42,48 +52,85 @@ $tbl = new CountryTable($countries, $filteringBy);
                 width: auto;
                 height: auto;
             }
+
+            .highlight {
+                background-color: #633100;
+            }
         </style>
     </head>
     <body>
         <form method="GET">
-            <label for="q">Country Search</label>
-            <input type="search" name="q" placeholder="term" value="<?=$input->term?>" />
+            <label for="q">Search Term</label>
+            <input type="search" name="q" placeholder="" value="<?=$input->term?>" />
             <input type="submit" value="Search" />
 
-            <input type="radio" name="c" value="0" <?=!$input->useCustomSearch ? "checked": "" ?> />
-            <label>API Search</label>
+            <fieldset>
+                <legend>Search Algorithm</legend>
 
-            <input type="radio" name="c" value="1" <?=$input->useCustomSearch ? "checked": "" ?> />
-            <label>Custom Search</label>
+                <input type="radio" name="c" value="0" <?=!$input->useCustomSearch ? "checked": "" ?> />
+                <label for="c">API</label>
 
-            <div>Search by</div>
+                <input type="radio" name="c" value="1" <?=$input->useCustomSearch ? "checked": "" ?> />
+                <label for="c">Custom</label>
+            </fieldset>
 
-            <input type="checkbox" name="s[]" value="<?=SearchBy::Name->value?>" <?=$input->isSearchingBy(SearchBy::Name) ? "checked": "" ?> />
-            <label>Name</label>
+            <fieldset> 
+                <legend>Search By </legend>
+                <input type="checkbox" name="s[]" value="<?=SearchBy::Name->value?>" <?=$input->isSearchingBy(SearchBy::Name) ? "checked": "" ?> />
+                <label for="s[]">Name</label>
 
-            <input type="checkbox" name="s[]" value="<?=SearchBy::Codes->value?>" <?=$input->isSearchingBy(SearchBy::Codes) ? "checked": "" ?> />
-            <label>Codes</label>
+                <input type="checkbox" name="s[]" value="<?=SearchBy::Codes->value?>" <?=$input->isSearchingBy(SearchBy::Codes) ? "checked": "" ?> />
+                <label for="s[]">Codes</label>
 
-            <input type="checkbox" name="s[]" value="<?=SearchBy::Currency->value?>" <?=$input->isSearchingBy(SearchBy::Currency) ? "checked": "" ?> />
-            <label>Currency</label>
+                <input type="checkbox" name="s[]" value="<?=SearchBy::Currency->value?>" <?=$input->isSearchingBy(SearchBy::Currency) ? "checked": "" ?> />
+                <label for="s[]">Currency</label>
 
-            <input type="checkbox" name="s[]" value="<?=SearchBy::Region->value?>" <?=$input->isSearchingBy(SearchBy::Region) ? "checked": "" ?> />
-            <label>Region</label>
-            
-            
-            <div>Filter by</div>
+                <input type="checkbox" name="s[]" value="<?=SearchBy::Region->value?>" <?=$input->isSearchingBy(SearchBy::Region) ? "checked": "" ?> />
+                <label for="s[]">Region</label>
+            </fieldset>
 
-            <input type="checkbox" name="f[]" value="<?=FilterBy::PopulationIsGreaterThan10M->value?>" <?=$input->isFilteringBy(FilterBy::PopulationIsGreaterThan10M) ? "checked" : ""?>/>
-            <label>Population > 10m</label>
+            <fieldset> 
+                <legend>Filter By </legend>
 
-            <input type="checkbox" name="f[]" value="<?=FilterBy::StartOfWeekIsSunday->value?>" <?=$input->isFilteringBy(FilterBy::StartOfWeekIsSunday) ? "checked" : ""?>/>
-            <label>Starts week on Sunday</label>
+                <input type="checkbox" name="f[]" value="<?=FilterBy::PopulationIsGreaterThan10M->value?>" <?=$tbl->isFilteringBy(FilterBy::PopulationIsGreaterThan10M) ? "checked" : ""?>/>
+                <label>Population > 10m</label>
 
+                <input type="checkbox" name="f[]" value="<?=FilterBy::StartOfWeekIsSunday->value?>" <?=$tbl->isFilteringBy(FilterBy::StartOfWeekIsSunday) ? "checked" : ""?>/>
+                <label>Starts week on Sunday</label>
+            </fieldset>
+
+            <fieldset> 
+                <legend>Sort By</legend>
+
+                <input type="radio" name="t" value="<?=SortBy::Name->value?>" <?=$tbl->isSortingBy(SortBy::Name) ? "checked" : ""?>/>
+                <label>Name</label>
+
+                <input type="radio" name="t" value="<?=SortBy::Population->value?>" <?=$tbl->isSortingBy(SortBy::Population) ? "checked" : ""?>/>
+                <label>Population</label>
+
+                <input type="radio" name="t" value="<?=SortBy::Region->value?>" <?=$tbl->isSortingBy(SortBy::Region) ? "checked" : ""?>/>
+                <label>Region</label>
+
+                <span> |</span>
+
+                <input type="radio" name="o" value="asc" <?=$tbl->sortOrder == SortOrder::Asc ? "checked": "" ?> />
+                <label>ASC</label>
+
+                <input type="radio" name="o" value="desc" <?=$tbl->sortOrder == SortOrder::Desc ? "checked": "" ?> />
+                <label>DESC</label>
+            </fieldset>
         </form>
+
+        <a href="/">Reset</a>
         
         <?php if ($input->term !== ""): ?>
         <table>
             <thead>
+                <tr>
+                    <th colspan="1">Total = <?=$tbl->getRowCount()?></th>
+                    <th colspan="2">Showing = <?=$tbl->getFilteredRowCount()?></th>
+                    <th colspan="2">Hidden = <?=$tbl->getFilteredOutRowCount()?></th>
+                </tr>
                 <tr>
                     <th>Country name</th>
                     <th>Population</th>
@@ -94,15 +141,24 @@ $tbl = new CountryTable($countries, $filteringBy);
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($tbl->getFilteredRows() as $row): ?>
+                <?php foreach ($tbl->getFilteredRows() as $row): 
+                    $name = new HighlightedText($row['name'], $input->term);
+                    $region = new HighlightedText($row['region'], $input->term);
+                    $subregion = new HighlightedText($row['subregion'], $input->term);
+                ?>
                     <tr>
-                        <td><?=$row['name']?></td>
+                        <td>
+                            <?=$name[0]?><span class="highlight"><?=$name[1]?></span><?=$name[2]?>
+                        </td>
                         <td><?=$row['population']?></td>
-                        <td><?=$row['region']?></td>
-                        <td><?=$row['subregion']?></td>
+                        <td>
+                            <?=$region[0]?><span class="highlight"><?=$region[1]?></span><?=$region[2]?>
+                        </td>
+                        <td>
+                            <?=$subregion[0]?><span class="highlight"><?=$subregion[1]?></span><?=$subregion[2]?>
+                        </td>
                         <td><?=$row['currency']?></td>
                         <td><img src="<?=$row['flag']['src']?>" alt="<?=$row['flag']['alt']?>"/></td>
-                        <td><?=$row['codes']?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
--- src/View.php
+++ src/View.php
@@ -2,6 +2,7 @@
 
 namespace XES\CodeChallenge\View;
 
+use ArrayAccess;
 use XES\CodeChallenge\Model\Country;
 use XES\CodeChallenge\Model\SearchBy;
 
@@ -11,15 +12,22 @@ enum FilterBy: string
     case StartOfWeekIsSunday = "start_of_wk_sun";
 }
 
+enum SortBy: string
+{
+    case Name = "name";
+    case Population = "population";
+    case Region = "region";
+}
+
+enum SortOrder: string
+{
+    case Asc = "asc";
+    case Desc = "desc";
+}
 
 class CountrySearchInput
 {
-    public function __construct(public readonly string $term, public readonly array $filteringBy = [], public readonly array $searchingBy = [], public readonly bool $useCustomSearch = false) { }
-
-    public function isFilteringBy(FilterBy $filterBy): bool
-    {
-        return in_array($filterBy, $this->filteringBy);
-    }
+    public function __construct(public readonly string $term = "", public readonly array $searchingBy = [], public readonly bool $useCustomSearch = false) { }
 
     public function isSearchingBy(SearchBy $searchBy): bool
     {
@@ -30,11 +38,17 @@ class CountrySearchInput
 
 class CountryTable 
 {
-    public function __construct(private readonly array $countries, public readonly array $filteringBy = []) { }
+    private ?array $rows = null;
+
+    public function __construct(private readonly array $countries, public readonly array $filteringBy = [], public readonly ?SortBy $sortBy = null, public readonly ?SortOrder $sortOrder = null) { } 
 
     public function getRows(): array
     {
-        $rows = array_map(fn($country) => [
+        if ($this->rows) {
+            return $this->rows;
+        }
+
+        $this->rows = array_map(fn($country) => [
             'name' => $country->getName(),
             'population' => $country->getPopulation(),
             'region' => $country->getRegion(),
@@ -44,9 +58,24 @@ class CountryTable
             'isFilteredOut' => $this->isFilteredOut($country)
         ], $this->countries);
 
-        usort($rows,  fn($a, $b) => strcmp($a['name'], $b['name']));
+        if ($this->sortBy) {
+            usort($this->rows,  fn($a, $b) => match ($this->sortBy) {
+                SortBy::Name => strcmp($a['name'], $b['name']),
+                SortBy::Population => $a['population'] - $b['population'],
+                SortBy::Region => $a['region'] !== $b['region'] ? strcmp($a['region'], $b['region']) : strcmp($a['subregion'], $b['subregion'])
+            });
+        }
 
-        return $rows;
+        if ($this->sortOrder === SortOrder::Desc) {
+            $this->rows = array_reverse($this->rows);
+        }
+
+        return $this->rows;
+    }
+
+    public function isFilteringBy(FilterBy $filterBy): bool
+    {
+        return in_array($filterBy, $this->filteringBy);
     }
 
     public function getFilteredRows(): array 
@@ -61,5 +90,81 @@ class CountryTable
             FilterBy::StartOfWeekIsSunday => strtolower($country->getStartOfWeek()) == "sunday", 
             default => true
         }, $this->filteringBy));
+    }
+
+    public function getRowCount(): int
+    {
+        return count($this->getRows());
+    }
+
+    public function getFilteredRowCount(): int
+    {
+        return count($this->getFilteredRows());
+    }
+
+    public function getFilteredOutRowCount(): int
+    {
+        return $this->getRowCount() - $this->getFilteredRowCount();
+    }
+
+    public function isSortingBy(SortBy $sortBy): bool
+    {
+        return $this->sortBy == $sortBy;
+    }
+}
+
+
+class HighlightedText implements ArrayAccess
+{
+    public readonly bool $isHighlighted;
+
+    public function __construct(public readonly string $text, public readonly string $term = "") 
+    { 
+        $this->isHighlighted = strpos(strtolower($this->text), strtolower($this->term)) !== false;
+    }
+
+    public function getOffset(): ?int
+    {
+        return $this->isHighlighted ? (int)strpos(strtolower($this->text), strtolower($this->term)) : null;
+    }
+
+    public function getPrefix(): string
+    {
+        return $this->isHighlighted ?  substr($this->text, 0, $this->getOffset()) : "";
+    }
+
+    public function getMatch(): string
+    {
+        return $this->isHighlighted ? substr($this->text, $this->getOffset(), strlen($this->term)) : "";
+    }
+
+    public function getSuffix(): string
+    {
+        return $this->isHighlighted ? substr($this->text, $this->getOffset() + strlen($this->term)) : $this->text;
+    }
+
+    public function getSlices(): array
+    {
+        return [$this->getPrefix(), $this->getMatch(), $this->getSuffix()];
+    }
+    
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->getSlices()[$offset];
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+       // read only
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return array_key_exists($offset, $this->getSlices());
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        // read only
     }
 }
